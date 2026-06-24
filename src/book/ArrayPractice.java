e26e29242174eb94ab4458f4e59975db224a29b7
gensokyoz1 <gensokyochen29@gmail.com>
2026-06-24 16:14:21 +0200

    added stuff

---
+++ src/book/ArrayPractice.java
@@ -0,0 +1,12 @@
+package book;
+import java.util.ArrayList;
+
+public class ArrayPractice {
+
+    int[] numbers = new int[5];
+    ArrayList<String> team = new ArrayList<>;
+    public void randomOne () {
+        numbers[1] = 3;
+    }
+
+}
--- src/examExercises/Manager.java
+++ src/examExercises/Manager.java
@@ -1,12 +1,22 @@
 package examExercises;
 
+import java.util.ArrayList;
+
 public class Manager extends Impiegato {
+    // Making team a literal field inside Manager
+    public ArrayList<ImpiegatoBase>team;
     public Manager (String nome, String cognome, int ore_annuali) {
         super (nome,cognome,ore_annuali);
+        this.team = new ArrayList<>();
     }
 
     @Override
     public double getStipendioAnnuale() {
         return this.getOre_annuali() * 25.90;
+        }
+
+        @Override
+        public boolean equals (Object obj) {
+        return super.equals(obj);
         }
 }
