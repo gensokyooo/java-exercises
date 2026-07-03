414729272da4a1b11a91942aa0b1e1ee59080401
gensokyoz1 <gensokyochen29@gmail.com>
2026-07-03 06:05:37 +0200

    Added Figurina

---
+++ src/examExercises/Figurina.java
@@ -0,0 +1,29 @@
+package examExercises;
+
+public abstract class Figurina {
+    private int id;
+
+    public Figurina (int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException ("id must be positive");
+        }
+        this.id = id;
+    }
+    public int getId () {
+        return id;
+    }
+
+    @Override
+    public boolean equals (Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Figurina)) return false;
+
+        Figurina other = (Figurina) obj;
+
+        boolean sameId = this.id == other.getId();
+
+        return sameId;
+
+    }
+}
