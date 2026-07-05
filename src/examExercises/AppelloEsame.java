4a6ddcd9607f25aad19e93ac096f956f6ac5775f
gensokyoz1 <gensokyochen29@gmail.com>
2026-07-05 16:20:57 +0200

    Kept working on AppelloEsame

--- src/examExercises/AppelloEsame.java
+++ src/examExercises/AppelloEsame.java
@@ -14,4 +14,26 @@ public class AppelloEsame {
         this.lavoratori = new StudenteLavoratore[50];
         this.ordinari = new StudenteOrdinario[50];
     }
+
+    public void iscriviti (Immatricolabile x) throws AppelloEsameException {
+
+        if (x instanceof StudenteOrdinario) {
+            for (int i = 0; i < ordinari.length ; i++) {
+                if (x.equals(ordinari[i])) {
+                    throw new AppelloEsameException("Student already subbed");
+                }
+            }
+        }
+
+        if (x instanceof StudenteLavoratore) {
+            for (int i = 0; i < lavoratori.length ; i++) {
+                if (x.equals(lavoratori[i])) {
+                    throw new AppelloEsameException("Student already subbed");
+                }
+            }
+        }
+
+
+
+    }
 }
---
+++ src/examExercises/AppelloEsameException.java
@@ -0,0 +1,7 @@
+package examExercises;
+
+public class AppelloEsameException extends Exception {
+    public AppelloEsameException (String message) {
+        super(message);
+    }
+}
